451ee59934d820ca8e0926ee33809b4146a38d4b
Bill Dunay <bdunay3@comcast.net>
2020-04-20 10:39:57 -0400

    Initial Commit

---
+++ JSONPlaceholder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JSONPlaceholder
+//
+//  Created by Bill Dunay on 4/20/20.
+//  Copyright © 2020 Bill Dunay. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
